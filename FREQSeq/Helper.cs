516515c0202176499218bc61ba976a14c5bf5815
Nigel Delaney <ndelaney@broadinstitute.org>
2013-12-03 16:03:05 -0500

    Many many many changes, renamed directories, etc.

---
+++ FREQSeq/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FREQSeq
+{
+    public class Helper
+    {
+
+        /// <summary>
+        /// The .gz extension to indicate gzipped files
+        /// </summary>
+        public const string ZippedFileExtension = ".gz";
+        public static bool FileEndsWithZippedExtension(string fileName)
+        {
+            return fileName.EndsWith(ZippedFileExtension);
+        }
+
+        
+			
+
+    }
+}
---
+++ FREQSeq/MiscMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace FREQSeq
+{
+    //A delegat to handle log events
+    public delegate void LogEventHandler(object sender,string Message);
+    public class MiscMethods
+    {
+        public static void ValidateACGT(string seq)
+        {
+            foreach (char c in seq)
+            {
+                switch (c)
+                {
+                    case 'A':
+                        break;
+                    case 'G':
+                        break;
+                    case 'C':
+                        break;
+                    case 'T':
+                        break;
+                    default:
+                        throw new IOException("Sequence: "+seq+" contains non A, C, G or T elements.");
+                }
+            }
+        }
+    }
+}
