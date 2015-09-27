a2fe3307b08477e081bacd8b5ac53b37250247b6
Nigel Delaney <ndelaney@smtp.nanofluidics.com>
2015-09-27 15:33:28 -0700

    Add windows and ubuntu build scripts

--- build.sh
+++ build.sh
@@ -17,3 +17,20 @@ cp freqout/bin/Release/* build/
 cd build
 export PKG_CONFIG_PATH=$HOME/mono64/lib/pkgconfig/
 mkbundle --keeptemp --static  --deps --config-dir /nothing --config /home/UNIXHOME/ndelaney/mono64/etc/mono/config -o freqout freqout.exe FreqSeq.dll
+cd ../
+
+# Make ubuntu distribution
+rm -fr freqseq_ubuntu; mkdir freqseq_ubuntu
+cp build/freqout freqseq_ubuntu
+cp Example.xml freqseq_ubuntu/
+cp ExampleCMD.sh freqseq_ubuntu/
+cp -r TestData freqseq_ubuntu/TestData
+tar -zcvf freqseq_ubuntu.tar.gz freqseq_ubuntu
+
+# Make Windows distribution
+rm -rf freqseq_win; mkdir freqseq_win
+cp build/*.dll freqseq_win/
+cp build/*.exe freqseq_win/
+cp Example.xml freqseq_win/
+cp -r TestData freqseq_ubuntu/TestData
+zip -r freqseq_win.zip freqseq_win/
