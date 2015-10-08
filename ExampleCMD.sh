d1e2061df84ad699b225fb34376688a82ca9ace5
Nigel Delaney <ndelaney@smtp.nanofluidics.com>
2015-10-08 12:09:00 -0700

    Allow libMonoPosixHelper.so to be distributed
    See my notes on accomplishing this here:
    http://stackoverflow.com/questions/18489272/how-can-i-convince-mkbundle-to-include-monoposixhelper/33023767#33023767

---
+++ ExampleCMD.sh
@@ -0,0 +1,1 @@
+ ./freqout -xml=TestData/freq2.xml TestData/Sample_1_run1_R1.fastq.gz
--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 rm -rf build; mkdir build
-
+rm freqseq_ubuntu.tar.gz
+rm freqseq_win.zip
 
 ### Build Bio dependency
 ## Note this requires having mono with the PCL assemblies installed into:
@@ -16,14 +17,19 @@ cp freqout/bin/Release/* build/
 # Now make a bundled executable
 cd build
 export PKG_CONFIG_PATH=$HOME/mono64/lib/pkgconfig/
-mkbundle --keeptemp --static  --deps --config-dir /nothing --config /home/UNIXHOME/ndelaney/mono64/etc/mono/config -o freqout freqout.exe FreqSeq.dll
+mkbundle --keeptemp --static  --deps --config-dir /nothing --config ../config -o freqout freqout.exe FreqSeq.dll
 cd ../
 
 # Make ubuntu distribution
+# See my stack overflow notes on how to deal with libMonoPosixHelper
+# at http://stackoverflow.com/questions/18489272/how-can-i-convince-mkbundle-to-include-monoposixhelper/33023767#33023767
+# 
 rm -fr freqseq_ubuntu; mkdir freqseq_ubuntu
 cp build/freqout freqseq_ubuntu
 cp Example.xml freqseq_ubuntu/
 cp ExampleCMD.sh freqseq_ubuntu/
+cp README_UBUNTU freqseq_ubuntu/README
+cp /home/UNIXHOME/ndelaney/mono64/lib/libMonoPosixHelper.so freqseq_ubuntu/
 cp -r TestData freqseq_ubuntu/TestData
 tar -zcvf freqseq_ubuntu.tar.gz freqseq_ubuntu
 
@@ -32,5 +38,5 @@ rm -rf freqseq_win; mkdir freqseq_win
 cp build/*.dll freqseq_win/
 cp build/*.exe freqseq_win/
 cp Example.xml freqseq_win/
-cp -r TestData freqseq_ubuntu/TestData
+cp -r TestData freqseq_win/TestData
 zip -r freqseq_win.zip freqseq_win/
